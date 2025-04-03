d815d44144e015a8d424a4a0e13e6fed0b839ea2
Andrew Johnson <andrewmjohnson549@gmail.com>
2025-04-03 12:47:43 +0200

    change pins

--- arduino-rotary-encoder.ino
+++ arduino-rotary-encoder.ino
@@ -1,11 +1,22 @@
 volatile int encoderPos = 0;
 volatile int lastEncoded = 0;
-const int pinA = 2; // Yellow wire, Channel A (CLK)
-const int pinB = 3; // Blue wire, Channel B (DT)
+
+// Pick pins carefully only  pins 0,1,2,3,7 can be used as interrupts
+// Pins 2/3 are also the only pins used for I2C
+// Pins 0/1 are also the only pins to be used as RX/TX UART
+// const int pinA = 2; // SDA Yellow wire, Channel A (CLK)
+// const int pinB = 3; // SCL Blue wire, Channel B (DT)
+const int pinA = 1; // TX Yellow wire, Channel A (CLK)
+// const int pinB = 0; // // Zero does not seem to work RX  Blue wire, Channel B (DT)
+const int pinB = 7; // // Zero does not seem to work RX  Blue wire, Channel B (DT)
+
+//int count = 1;
 
 void setup() {
   pinMode(pinA, INPUT_PULLUP);
   pinMode(pinB, INPUT_PULLUP);
+  // attachInterrupt(digitalPinToInterrupt(pinA), updatePinA, CHANGE);
+  // attachInterrupt(digitalPinToInterrupt(pinB), updatePinB, CHANGE);
   attachInterrupt(digitalPinToInterrupt(pinA), updateEncoder, CHANGE);
   attachInterrupt(digitalPinToInterrupt(pinB), updateEncoder, CHANGE);
   Serial.begin(9600);
@@ -13,8 +24,21 @@ void setup() {
 
 void loop() {
   Serial.println(encoderPos);
+  // Serial.println("IN LOOP");
+  // Serial.println(count);
+  // count ++;
   delay(10);
 }
+
+// void updatePinA()
+// {
+//   Serial.println("A Updated");
+// }
+
+// void updatePinB()
+// {
+//   Serial.println("B Updated"); 
+// }
 
 void updateEncoder() {
   // Read both channels
